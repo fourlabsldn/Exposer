3bbb420c5ce6e25459d081fc9af27ce42ca446e4
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-02-25 14:46:33 +0000

    Add build image

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Exposer
+[![Build Status](https://travis-ci.org/lazamar/helloTravis.svg?branch=master)](https://travis-ci.org/lazamar/helloTravis)
 Module to expose private functions of a module
--- tests/exposer_spec.js
+++ tests/exposer_spec.js
@@ -1,5 +1,38 @@
-describe("testing 1", function () {
+describe("Exposer module", function () {
+
+  //Mock module
+  function MockModule (var1, var2) {
+    function private1() {}
+
+    function private2 () {}
+
+    function private3 (){}
+
+    function private4(){}
+
+    var private5 = function () {};
+
+    var private6 = (function () {}());
+
+    var private7 = (function () {}());
+
+    //This is actually global, but we must account for that too.
+    private8 = (function () {})();
+
+    this.public1 = function () {};
+
+    this.public2 = function public2 () {};
+
+    this.public3 = (function public3(){});
+
+    this.public4 = (function (){});
+
+    return this;
+  }
+
   it("Should have loaded the module", function () {
     expect(Exposer).toBeDefined();
-  })
+  });
+
+  it("")
 })
