66477a11a3dc3a598503a5d933f84fa7687a1efe
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-02-26 10:39:33 +0000

    Pass all tests

--- tests/exposer_spec.js
+++ tests/exposer_spec.js
@@ -59,22 +59,22 @@ var mockmodule = function MockModule (var1, var2) {
 describe('Exposer module', function () {
   'use strict';
 
-  it('Should have been loaded by Jasmine', function () {
+  it('should have been loaded by Jasmine', function () {
     expect(Exposer).toBeDefined();
   });
 
-  it('Should be successfully instantiated without parameters', function () {
+  it('should be successfully instantiated without parameters', function () {
     var ex = new Exposer();
     expect(ex).toBeDefined();
-    expect(typeof ex === 'function').toBe(true);
+    expect(typeof ex === 'object').toBe(true);
   });
 
-  it('Should have an exposeAll function', function () {
+  it('should have an exposeAll function', function () {
     var ex = new Exposer();
     expect(typeof ex.exposeAll === 'function').toBe(true);
   });
 
-  describe('When exposing a module', function () {
+  describe('when exposing a module', function () {
 
 
     var exposer = new Exposer();
@@ -83,49 +83,55 @@ describe('Exposer module', function () {
     //will be created will use the real function name.
     exposer.exposeAll(mockmodule);
 
-    it('Should generate a global variable with the function name', function () {
+    it('should generate a global variable with the function name', function () {
       expect(MockModule).toBeDefined();
     });
 
+    it('This global variable should be a function', function () {
+      expect(typeof MockModule === 'function').toBe(true);
+    });
+
     var exposedModule = new MockModule();
-    it('Should have constructed an object of the right type', function () {
-      expect(typeof exposedModule === 'function').toBe(true);
+    it('should have constructed an object of the right type', function () {
+      expect(typeof exposedModule === 'object').toBe(true);
     });
 
-    it('Should have exposed the right amount of functions', function () {
-      expect(Object.keys(exposedModule)).toEqual(12);
+    it('should have constructed an object with the right constructor', function () {
+      expect(exposedModule instanceof MockModule).toBe(true);
     });
 
-    it('Should have added an underscore to private methods', function () {
+    it('should have exposed the right amount of functions', function () {
+      expect(Object.keys(exposedModule).length).toEqual(12);
+    });
+
+    it('should have added an underscore to private methods', function () {
       expect(typeof exposedModule._private1 === 'function').toBe(true);
     });
 
-    it('Should call the private methods appropriately', function () {
+    it('should call the private methods appropriately', function () {
       expect(exposedModule._private1()).toEqual('worked');
-      expect(exposedModule._private1(1, 1)).toEqual(2);
+      expect(exposedModule._private2(1, 1)).toEqual(2);
     });
 
-    it('Should not have exposed again functions that were already public', function () {
+    it('should not have exposed again functions that were already public', function () {
       expect(exposedModule._public1).not.toBeDefined();
       expect(exposedModule._this).not.toBeDefined();
       expect(exposedModule.this).not.toBeDefined();
-      expect(exposedModule.this.public1).toThrow();
-      expect(exposedModule._this.public1).toThrow();
     });
 
-    it('Should not expose function declarations in comments', function () {
+    it('should not expose function declarations in comments', function () {
       expect(exposedModule._inlineCommentFunction1).not.toBeDefined();
       expect(exposedModule.inlineCommentFunction1).not.toBeDefined();
       expect(exposedModule._blockCommentFunction).not.toBeDefined();
       expect(exposedModule.blockCommentFunction).not.toBeDefined();
     });
 
-    it('Should not expose function declarations within strings', function () {
+    it('should not expose function declarations within strings', function () {
       expect(exposedModule._stringFunction).not.toBeDefined();
       expect(exposedModule.stringFunction).not.toBeDefined();
     });
 
-    it('Should not expose private varibles', function () {
+    it('should not expose private varibles', function () {
       expect(exposedModule._stringVariable).not.toBeDefined();
       expect(exposedModule._numberVariable).not.toBeDefined();
       expect(exposedModule._objectVariable).not.toBeDefined();
@@ -136,14 +142,14 @@ describe('Exposer module', function () {
       expect(exposedModule.objectWithFunction).not.toBeDefined();
     });
 
-    it('Should not expose functions within objects', function () {
+    it('should not expose functions within objects', function () {
       expect(exposedModule.objectFunction1).not.toBeDefined();
       expect(exposedModule._objectFunction1).not.toBeDefined();
       expect(exposedModule.objectFunction2).not.toBeDefined();
       expect(exposedModule._objectFunction2).not.toBeDefined();
     });
 
-    it('Should not expose immediate functions not assigned to a variable', function () {
+    it('should not expose immediate functions not assigned to a variable', function () {
       expect(exposedModule._immediateFunctionNotExportable).not.toBeDefined();
       expect(exposedModule.immediateFunctionNotExportable).not.toBeDefined();
     });
